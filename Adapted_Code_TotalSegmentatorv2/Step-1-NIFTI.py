67713b1cb3e85ad7d73f8180956251929342a4b0
Quincy Alexander Hathaway <47356848+qahathaway@users.noreply.github.com>
2025-01-19 13:58:32 -0500

    Update and rename NIFTI.py to Step-1-NIFTI.py

--- Adapted_Code_TotalSegmentatorv2/Step-1-NIFTI.py
+++ Adapted_Code_TotalSegmentatorv2/Step-1-NIFTI.py
@@ -14,7 +14,7 @@ import dicom2nifti # to convert DICOM files to the NIftI format
 import nibabel as nib # nibabel to handle nifti files
 from pathlib import Path # pathlib for easy path handling
 
-folder = '/home/sdemehr1/scr16_sdemehr1/MESA_Exam6'
+folder = '/path/to/main/DICOM/repository'
 sub_folders = [name for name in os.listdir(folder) if os.path.isdir(os.path.join(folder, name))]
 sortedfolder = sorted(sub_folders)
 
@@ -22,12 +22,12 @@ for q in sortedfolder:
     files = []
     
     try:
-        # Automatically Create PNG from DICOM for All Patients in Directory #
-        for filename in sorted(glob.glob('/home/sdemehr1/scr16_sdemehr1/MESA_Exam6/' + q + '/*')):
+        # Create NIFTI files from DICOM for All Patients in Directory #
+        for filename in sorted(glob.glob('/path/to/main/DICOM/repository/' + q + '/*')):
             files.append(pydicom.dcmread(filename, force=True))
 
         try:
-            dicom2nifti.convert_dicom.dicom_array_to_nifti(files, '/home/sdemehr1/data_sdemehr1/MESA_Exam6_NIFTI/'+ q)
+            dicom2nifti.convert_dicom.dicom_array_to_nifti(files, '/path/to/main/output/NIFIT/directory/'+ q)
         except:
             pass
 
